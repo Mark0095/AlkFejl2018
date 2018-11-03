916efcbf717d4d3d65b5bd591206fe98a645a5ba
zsike888888 <zsike888888@gmail.com>
2018-11-03 19:28:55 +0100

    teszt

--- news/src/main/data.sql
+++ news/src/main/data.sql
@@ -1,3 +1,13 @@
-INSERT INTO USER(NAME,USER_ID,PASSWORD) VALUES ('Szilagyi Erzsebet', 0001, 'jelszo');
-INSERT INTO USER(NAME,USER_ID,PASSWORD) VALUES ('Szilagyi Gabor', 0002, 'jelszo123');
+INSERT INTO USER(NAME,USER_ID,PASSWORD, USERNAME, ROLE) VALUES ('Szilágyi Erzsébet', 1, 'jelszo', 'zsike123', 'ROLE_ADMIN');
+INSERT INTO USER(NAME,USER_ID,PASSWORD, USERNAME, ROLE) VALUES ('Szilágyi Gábor', 2, 'jelszo123', 'gabor93', 'ROLE_USER');
+INSERT INTO USER(NAME,USER_ID,PASSWORD, USERNAME, ROLE) VALUES ('Szalontai Márk', 3, 'jelszo12345', 'mark12345', 'ROLE_ADMIN');
+INSERT INTO USER(NAME,USER_ID,PASSWORD, USERNAME, ROLE) VALUES ('Molnár Krisztián', 4, 'kiki10', 'kiki10', 'ROLE_USER');
 select * from USER;
+
+INSERT INTO ARTICLE(ARTICLE_ID, TITLE, AUTHOR, DATE, SUMMARY, FULL_ARTICLE, ISLEADINGARTICLE) VALUES (1,'Cim', 'Szilágyi Gábor', '2008-11-11', 'Ez egy rövid összefoglaló a cikkről', 'Ez a cikk teljes tartalma', FALSE);
+
+select * from ARTICLE;
+
+INSERT INTO PICTURE(ARTICLE_ID,URL) VALUES ('1', 'valami.jpg');
+
+select * from PICTURE;
