f9e70da9647bd8649b2fd0d8444338e53b714278
zsike888888 <zsike888888@gmail.com>
2018-11-03 19:06:26 +0100

    teszt

--- news/src/main/data.sql
+++ news/src/main/data.sql
@@ -1,1 +1,3 @@
-select * from FELHASZNALO;
+INSERT INTO USER(NAME,USER_ID,PASSWORD) VALUES ('Szilagyi Erzsebet', 0001, 'jelszo');
+INSERT INTO USER(NAME,USER_ID,PASSWORD) VALUES ('Szilagyi Gabor', 0002, 'jelszo123');
+select * from USER;
